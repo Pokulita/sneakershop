b1e01e02527b91fd6afd79ad30213e1f95167487
pokulita <nikitasl2208@gmail.com>
2024-09-07 21:32:42 +0200

    added get request

--- sneakershop/src/App.js
+++ sneakershop/src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 import Home from "./pages/Home";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Fetch data from the Spring Boot backend
+    axios
+      .get("http://localhost:8080/api/hello
+        ")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
   return (
     <>
       <Home></Home>
